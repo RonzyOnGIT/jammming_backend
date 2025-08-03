3121988acd9c86267811d68bf5aa7867b2bf6981
= <ninjakai25@gmail.com>
2025-08-02 21:39:02 -0500

    return json object from api endpoint

--- src/main/java/com/jammming/backend/controllers/SpotifyLoginController.java
+++ src/main/java/com/jammming/backend/controllers/SpotifyLoginController.java
@@ -1,5 +1,10 @@
 package com.jammming.backend.controllers;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +12,12 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.jammming.backend.services.SpotifyLoginService;
 
+// frontend makes fetch request to return a string url then redirect there
+// after using logs in to spotify redirect back to another redirect
+// in the endpoint, redirect user back to spotify using logged in or something by doing return new RedirectView("");
+
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class SpotifyLoginController {
@@ -18,11 +29,27 @@ public class SpotifyLoginController {
     }
     
     // builds url and returns string url so the front-end can redirect users to spotify's auth page
+    //  have to come back to add other possible error codes
     @GetMapping("/login")
-    public RedirectView getLoginUrl() {
+    public ResponseEntity<Map<String, String>> getLoginUrl() {
 
-        return this.spotifyLoginService.getLoginUrl();
-        
+        String stateId = this.spotifyLoginService.getStateId();
+
+        Map<String, String> response = Map.of(
+            "state", stateId,
+            "url", this.spotifyLoginService.getLoginUrl(stateId)
+        );
+
+        return ResponseEntity.ok(response);
+
     }
+
+    // user will get redirected to this endpoint
+    @GetMapping("/redirect")
+    public RedirectView handleSpotifyRedirect() {
+       return new RedirectView("http://localhost:3000/loggedin");
+
+    }
+
     
 }
--- src/main/java/com/jammming/backend/services/SpotifyLoginService.java
+++ src/main/java/com/jammming/backend/services/SpotifyLoginService.java
@@ -14,23 +14,29 @@ public class SpotifyLoginService {
     @Value("${clientId}")
     private String clientId;
     
-    // builds url and returns string url so the front-end can redirect users to spotify's auth page
-    public RedirectView getLoginUrl() {
+    // returns an array where first element is the state id and second element is the url to direct user to login to spotify account
+    public String getLoginUrl(String state) {
 
         String fallbackUrl = "http://localhost:5173/";
 
         String responseType = "code";
 
         String scope = "playlist-modify-public playlist-modify-private user-read-private";
-    
-        UUID uniqueId = UUID.randomUUID();
 
-        // state will be for added security when redirecting user and back
-        String state = uniqueId.toString();
-
+        // state will be passed down to user, when a user gets to the login page will be sent the state, then once the user comes back from the redirect to fallback, check that the state matches, if it does not match, means there was some interception in between, and do not grant access to user
         String loginUrl = "https://accounts.spotify.com/authorize?response_type=" + responseType + "&client_id=" + clientId + "&scope=" + scope + "&redirect_uri=" + fallbackUrl + "&state=" + state;
 
-        return new RedirectView(loginUrl);
+        return loginUrl;
+
+    }
+
+    public String getStateId() {
+
+        UUID uniqueId = UUID.randomUUID();
+        String state = uniqueId.toString();
+
+        return state;
+        
     }
 
 }
