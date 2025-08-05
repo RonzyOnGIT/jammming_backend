1ed0f7184ceb87cf878bdf689da4a4bd05871176
= <ninjakai25@gmail.com>
2025-08-04 21:28:36 -0500

    finish working on redirect endpoint

--- src/main/java/com/jammming/backend/controllers/SpotifyLoginController.java
+++ src/main/java/com/jammming/backend/controllers/SpotifyLoginController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.server.ResponseStatusException;
 
 
 import com.jammming.backend.services.SpotifyLoginService;
@@ -33,6 +34,7 @@ public class SpotifyLoginController {
     
     // builds url and returns string url so the front-end can redirect users to spotify's auth page
     //  have to come back to add other possible error codes
+    // for some reason, cant even hit this api from the frontend, has to do with interaction between frontend and the backend
     @GetMapping("/login")
     public ResponseEntity<Map<String, String>> getLoginUrl(HttpServletRequest request) {
 
@@ -49,27 +51,28 @@ public class SpotifyLoginController {
 
     }
 
-    // user will get redirected to this endpoint
     @GetMapping("/redirect")
     public RedirectView handleSpotifyRedirect(@RequestParam(value = "code", required = false) String code, @RequestParam(value = "state", required = true) String state, @RequestParam(value = "error", required = false) String error, HttpServletRequest request) {
         
-        // 4
-        // first case is that the user accepted the terms and state matches
-        // first check that the state matches, because no matter what if mismatch of states, then probably not a good thing
+        // state from redirect does not match the original state that was passed into the original redirect, fishy stuff
         if (request.getSession().getAttribute("spotify_state") != state) {
-            // state mismatch, return some message saying error
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid state parameter");
         }
 
+        StringBuilder redirectUrl = new StringBuilder("http://localhost:3000");
+
+        // user accepted conditions and matching states
+        if (code != null) {
+            // redirect back to frontend application with "/logged_in" url or something to show that logged in with returned code 
+            redirectUrl.append("?code=").append(code);
+        }
+
+        // some error happened or user declined conditions
+        if (error != null) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, error);
+        }
         
-
-        // second case is that the user accepted but state no match
-
-        // 3rd case is that the user did not accept and the state match
-        // 4th case is that the user did not accept and state no match 
-        // case 3 and 4 can be bundled up into of if() block because either way user declined
-
-        // check to see if the state that was passed in matches state 
-       return new RedirectView("http://localhost:3000/loggedin");
+        return new RedirectView(redirectUrl.toString());
 
     }
 
